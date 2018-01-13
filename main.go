bea6a787b287bae76f8c8ffc04689306517eba44
Keenan Gray <krgray@wpi.edu>
2018-01-12 21:55:29 -0500

    got a room code to display on the webpage

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -29,9 +32,14 @@ func main() {
 	})
 
 	router.GET("/hostgame", func(c *gin.Context) {
-		queue.PrintUsers()
-		fmt.Print("FJ")
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		l := queue.GenerateLobby()
+
+		fmt.Println("Hey")
+		l.PrintLobby()
+
+		GetHostPage(l)
+
+		c.HTML(http.StatusOK, "host_page.tmpl.html", nil)
 	})
 
 	router.GET("/joingame", func(c *gin.Context) {
@@ -39,4 +47,37 @@ func main() {
 	})
 
 	router.Run(":" + port)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func GetHostPage(l queue.Lobby) {
+	// Create a new template and parse the letter into it.
+	t := template.Must(template.New("host_page_template.tmpl").ParseFiles("templates/host_page_template.tmpl"))
+
+	var webpage bytes.Buffer
+
+	// Execute the template for each recipient.
+	/*
+		for _, r := range Projects {
+			err := t.Execute(&webpage, r)
+			if err != nil {
+				log.Println("executing template:", err)
+			}
+		}
+	*/
+
+	err := t.Execute(&webpage, l)
+	if err != nil {
+		log.Println("executing template:", err)
+	}
+
+	var OutputFile = "templates/host_page.tmpl.html"
+	d1 := webpage.Bytes()
+	err = ioutil.WriteFile(OutputFile, d1, 0644)
+	check(err)
 }
--- queue/lobby.go
+++ queue/lobby.go
@@ -4,26 +4,33 @@ import "fmt"
 
 //Lobby is a list of players currently in a game
 type Lobby struct {
-	users []string
-	code  string
+	Users []string
+	Code  string
 }
 
-/*GenerateLobby creates a new lobby with an empty list of users and unique lobby code
+/*GenerateLobby creates a new lobby with an empty list of Users and unique lobby Code
 
  */
 func GenerateLobby() Lobby {
-	l := Lobby{users: make([]string, 3), code: createLobbyCode()}
+	l := Lobby{Users: make([]string, 3), Code: createLobbyCode()}
 
 	return l
 }
 
-func (r *Lobby) addUser(name string) int {
+/*
+PrintLobby prints the information fields of the structure for debugging
+*/
+func (lob_Instance *Lobby) PrintLobby() {
+	fmt.Println(lob_Instance.Code)
+}
+
+func (lob_Instance *Lobby) addUser(name string) int {
 	fmt.Println(name)
 	return 0
 }
 
 /*
-TODO: create randomized lobby code
+TODO: create randomized lobby Code
 */
 func createLobbyCode() string {
 	return "ASDF"
@@ -32,7 +39,6 @@ func createLobbyCode() string {
 /*
 TODO: REMOVE THIS TESTING FUNCTION
 */
-
 func PrintUsers() {
 	fmt.Println("something happened")
 }
